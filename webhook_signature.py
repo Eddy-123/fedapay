154d7d8d4183cf833041c8ea36405c8cf5c6801f
Eddy Adegnandjou <adegnandjoueddy12@gmail.com>
2024-02-16 13:32:31 +0100

    compute signature

--- webhook_signature.py
+++ webhook_signature.py
@@ -1,21 +1,34 @@
+from abc import ABCMeta
+import hmac
+import hashlib
 
-
-class WebhookSignature:
+class WebhookSignature(metaclass=ABCMeta):
+    EXPECTED_SCHEME = 's'
     
-    def __init__(self):
-        self.expected_scheme = 's'
-    
-    def verify_header(self, payload, signature_header, secret, tolerance):
-        timestamp = self.get_timestamp(signature_header)
-        signatures = self.get_signatures(signature_header, scheme=self.expected_scheme)
+    @classmethod
+    def verify_header(cls, payload, header, secret, tolerance=None):
+        timestamp = cls.__get_timestamp(header)
+        signatures = cls.__get_signatures(header, scheme=cls.EXPECTED_SCHEME)
+        print('signatures=', signatures)
         if timestamp == -1:
+            # todo: raise exception
             print('SignatureVerification Error: Unable to extract timestamp and signatures from header')
         
         if not signatures:
+            # todo: raise exception
             print('SignatureVerification Error: No signatures found with expected scheme')
         
-    def get_timestamp(self, signature_header):
-        items = signature_header.split(',')
+        signed_payload = timestamp + '.' + payload
+        expected_signature = cls.__compute_signature(signed_payload, secret)
+        signature_found = False
+        for signature in signatures:
+            # secure_compare
+            pass
+        
+        
+    @staticmethod
+    def __get_timestamp(header):
+        items = header.split(',')
         for item in items:
             item_parts = item.split('=', maxsplit=1)
             if item_parts[0] == 't':
@@ -24,13 +37,21 @@ class WebhookSignature:
                 return int(item_parts[1])
         return -1
     
-    def get_signatures(self, signature_header, scheme):
+    @staticmethod
+    def __get_signatures(header, scheme):
         signatures = []
-        items = signature_header.split(',')
+        items = header.split(',')
         for item in items:
             item_parts = item.split('=', maxsplit=1)
             if item_parts[0] == scheme:
                 signatures.append(item_parts[1])
         return signatures
         
-            
+    @staticmethod
+    def __compute_signature(payload, secret):
+        return hmac.new(
+            bytes(secret, 'latin-1'), 
+            msg=bytes(payload, 'latin-1'), 
+            digestmod=hashlib.sha256
+            ).hexdigest()
+        
